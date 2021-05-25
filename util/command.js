d4d1f10eae3913974b7a354a11ce5055f938912d
Altersplatz <78807470+Altersplatz@users.noreply.github.com>
2021-05-25 12:50:23 +0530

    Added /util folder

---
+++ util/command.js
@@ -0,0 +1,30 @@
+const { prefix } = require('@root/config.json')
+
+module.exports = (client , aliases , callback) => {
+
+    if(typeof aliases === 'string'){
+
+        aliases = [aliases]
+
+    }
+
+    client.on('message' , (message) => {
+
+        const { content } = message
+
+        aliases.forEach((alias) => {
+
+            const command = `${prefix}${alias}`
+
+            if(content.startsWith(`${command}`) || content === command){
+
+                console.log(`Running the message ${command}`);
+                callback(message)
+        
+            }
+    
+        })
+
+    })
+
+}
